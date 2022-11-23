b81723e11c830c0df882fa452b2f8aa4e106bde7
Moyrasc <noemisolac@gmail.com>
2022-11-23 18:05:04 +0100

    primeros pasos php

---
+++ 02-variables.php
@@ -0,0 +1,21 @@
+<?php include 'includes/header.php';
+
+
+$nombre = "Noe";
+
+
+$nombre = "Noe Sola";
+
+echo $nombre;
+var_dump($nombre);
+
+define('constante', "Este es el valor de la constante");
+echo constante;
+
+const constante2 = "Hola Mundo";
+echo constante2;
+
+$nombreCliente = "Pedro";
+$nombre_cliente = "Pedro";
+
+include 'includes/footer.php';
